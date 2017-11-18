47a16c3fb03e0f75c1eb63ed68733cc1146537b8
Robert Fletcher <lobatifricha@gmail.com>
2017-11-18 14:35:44 -0800

    Bugs: pass block down to Callable instance method
    This fixes an issue where using the class level `call` method cannot
    be used with a block.

--- lib/junk_drawer/callable.rb
+++ lib/junk_drawer/callable.rb
@@ -19,8 +19,8 @@ module JunkDrawer
     # an instance. It also causes an error to be raised if a public instance
     # method is defined with a name other than `call`
     module ClassMethods
-      def call(*args)
-        new.(*args)
+      def call(*args, &block)
+        new.(*args, &block)
       end
 
       def to_proc
