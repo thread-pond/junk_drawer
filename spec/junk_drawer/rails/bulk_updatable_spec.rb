f820373e3a7c958bf5df8a696e4ebbc491249f14
Robert Fletcher <mockdeep@users.noreply.github.com>
2018-03-16 15:45:04 -0700

    BulkUpdatable: extract specs to shared examples (#6)
    I'm going to be adding a lot more types to be tested against here, so
    wanted to make them a little more generic. This adds a new set of shared
    examples and runs them against each type. I added a `GENERATORS` hash to
    generate data for each type and a couple of helper methods in the test.
    One weird thing is that in order to access `type` in the methods, I
    needed to set it in a `let`, otherwise it would throw a `NoMethodError`.

--- spec/junk_drawer/rails/bulk_updatable_spec.rb
+++ spec/junk_drawer/rails/bulk_updatable_spec.rb
@@ -23,12 +23,7 @@ RSpec.describe JunkDrawer::BulkUpdatable, '.bulk_update' do
     end
   end
 
-  let(:models) do
-    [
-      BulkUpdatableModel.create!(string_value: ''),
-      BulkUpdatableModel.create!(string_value: ''),
-    ]
-  end
+  let(:models) { [BulkUpdatableModel.create!, BulkUpdatableModel.create!] }
 
   it 'updates the attribute on all the models' do
     models.each_with_index do |model, index|
@@ -68,55 +63,6 @@ RSpec.describe JunkDrawer::BulkUpdatable, '.bulk_update' do
     end.not_to raise_error
   end
 
-  it 'works for hstore datatypes' do
-    models.each_with_index do |model, index|
-      model.hstore_value = { "key_#{index}": "thing_data_#{index}" }
-    end
-
-    BulkUpdatableModel.bulk_update(models)
-    models.each(&:reload)
-
-    expect(models[0].hstore_value).to eq('key_0' => 'thing_data_0')
-    expect(models[1].hstore_value).to eq('key_1' => 'thing_data_1')
-  end
-
-  it 'works for jsonb datatypes' do
-    models.each_with_index do |model, index|
-      model.jsonb_value = { key: index }
-    end
-
-    BulkUpdatableModel.bulk_update(models)
-    models.each(&:reload)
-
-    expect(models[0].jsonb_value).to eq('key' => 0)
-    expect(models[1].jsonb_value).to eq('key' => 1)
-  end
-
-  it 'works for boolean datatypes' do
-    models.first.boolean_value = true
-    models.last.boolean_value = false
-
-    BulkUpdatableModel.bulk_update(models)
-    models.each(&:reload)
-
-    expect(models.first.boolean_value).to be true
-    expect(models.last.boolean_value).to be false
-  end
-
-  it 'works for datetime datatypes' do
-    now = Time.zone.now.round
-    before = 3.days.ago.round
-
-    models.first.datetime_value = now
-    models.last.datetime_value = before
-
-    BulkUpdatableModel.bulk_update(models)
-    models.each(&:reload)
-
-    expect(models.first.datetime_value.round).to eq now
-    expect(models.last.datetime_value.round).to eq before
-  end
-
   it 'can bulk update multiple columns at once' do
     models.first.boolean_value = true
     models.first.string_value = 'weeehooo'
@@ -130,5 +76,9 @@ RSpec.describe JunkDrawer::BulkUpdatable, '.bulk_update' do
     expect(models.first.string_value).to eq 'weeehooo'
     expect(models.last.boolean_value).to be false
     expect(models.last.string_value).to eq 'plop'
+  end
+
+  DATA_TYPES.each do |type|
+    it_behaves_like 'bulk updatable type', type
   end
 end
